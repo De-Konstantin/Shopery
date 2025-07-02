a41677004c3224f4415d1b24609713140e06e41f
De-Konst <denkakonstantinov@gmail.com>
2025-07-02 11:09:54 +0300

    icomoon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Home() {
-  return <div>какая-то надпсь!!!!!!!!!!!</div>;
+  return (
+    <div>
+      какая-то надпсь!!!!!!!!!!! span{' '}
+      <span className="icon-apple">Ecobazar</span>
+    </div>
+  );
 }
 
 export default Home;
