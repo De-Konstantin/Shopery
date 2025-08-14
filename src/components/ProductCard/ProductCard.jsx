7561981bf983cba0a478339a6eaeb61907602140
De-Konst <denkakonstantinov@gmail.com>
2025-08-14 17:16:39 +0300

    redirect to public

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,39 +1,44 @@
 import React from 'react';
 import styles from './ProductCard.module.scss';
-function ProductCard({ product }) {
+import data from '../../utils/products.json';
+function ProductCard({ product1 }) {
+  const product = data[0];
+
   return (
     <div className={styles.productCard}>
-      {product.discount && (
+      {product.discount ? (
         <span className={styles.productCard__badge}>
-          Sale {product.discount}%
+          Sale {product.discount}%{' '}
         </span>
-      )}
+      ) : null}
       <img
-        src={product.image}
-        alt={product.name}
+        src={product.image[0]}
+        alt={product.productName}
         className={styles.productCard__image}
       />
-      <h3 className={styles.productCard__name}>{product.name}</h3>
+      <h3 className={styles.productCard__name}>
+        {product.productName}
+      </h3>
       <p className={styles.productCard__priceBlock}>
-        {product.discountPrice ? (
+        {product.discount ? (
           <>
             <span className={styles.productCard__price}>
-              ${product.discountPrice}
-            </span>
+              ${product.discount}
+            </span>{' '}
             <span className={styles.productCard__oldPrice}>
-              ${product.price}
+              ${product.priceOrigin}
             </span>
           </>
         ) : (
           <span className={styles.productCard__price}>
-            ${product.price}
+            ${product.priceOrigin}
           </span>
         )}
-      </p>
+      </p>{' '}
       <div className={styles.productCard__rating}>
         {'★'.repeat(Math.floor(product.rating))}
         {'☆'.repeat(5 - Math.floor(product.rating))}
-      </div>
+      </div>{' '}
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,17 @@ import SubcribeBanner from '../components/SubcribeBanner/SubcribeBanner';
 import Hero from '../components/Hero/Hero';
 import Logos from '../components/Logos/Logos';
 import TopCategory from '../components/TopCategory/TopCategory';
+import ProductCard from '../components/ProductCard/ProductCard';
 
 function Home() {
+  console.log(ProductCard);
+
   return (
     <div>
       <span className="icon-apple">
         <Hero />
         <TopCategory />
-
+        <ProductCard />
         <SubcribeBanner />
         <Logos />
       </span>
